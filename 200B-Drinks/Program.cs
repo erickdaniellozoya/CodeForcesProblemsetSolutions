30eb76ca3ce615d5cc6b1200bf3ca2858831623c
Erick Daniel Lozoya Rosas <erick_dany97@hotmail.com>
2024-04-10 00:22:56 -0600

    Initial commit

---
+++ 200B-Drinks/Program.cs
@@ -0,0 +1,5 @@
+﻿int n = int.Parse(Console.ReadLine());
+int pSum = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse).Sum();
+int maxPer = 100 * n;
+decimal res = 100m * (pSum * 1m) / (maxPer * 1m);
+Console.WriteLine(res.ToString("n12"));
