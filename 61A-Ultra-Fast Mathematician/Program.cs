e33e1330cae0837d6ee90df949d89ff43bd75073
Erick Daniel Lozoya Rosas <erick_dany97@hotmail.com>
2024-04-14 02:02:25 -0600

    Problem 61A-Ultra-Fast Mathematician solved

---
+++ 61A-Ultra-Fast Mathematician/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+string line1 = Console.ReadLine();
+string line2 = Console.ReadLine();
+StringBuilder sb = new StringBuilder();
+for (int i = 0; i < line1.Length; i++)
+{
+    sb.Append(line1[i] == line2[i] ? '0' : '1');
+}
+Console.WriteLine(sb.ToString());
